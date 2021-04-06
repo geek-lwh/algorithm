5056a21379cfd5f708c1f05fbb32513a86dca123
luweihong <luweihong@ahaschool.com>
2021-04-06 19:23:18 +0800

    链表反转

---
+++ src/main/java/list/ListNode.java
@@ -0,0 +1,32 @@
+package list;
+
+/**
+ * @Author: luweihong
+ * @Date: 2021/4/6
+ */
+public class ListNode {
+    int val;
+    list.ListNode next;
+    ListNode(int x) { val = x; }
+
+    public static ListNode simpleListNode(){
+        ListNode listNode1 = new ListNode(1);
+        ListNode listNode2 = new ListNode(2);
+        ListNode listNode3 = new ListNode(3);
+        ListNode listNode4 = new ListNode(4);
+        ListNode listNode5 = new ListNode(5);
+        listNode1.next = listNode2;
+        listNode2.next = listNode3;
+        listNode3.next = listNode4;
+        listNode4.next = listNode5;
+
+        return listNode1;
+    }
+
+    public static void print(ListNode listNode){
+        while (listNode != null){
+            System.out.println(listNode.val);
+            listNode = listNode.next;
+        }
+    }
+}
--- src/main/java/list/Reverse.java
+++ src/main/java/list/Reverse.java
@@ -4,8 +4,27 @@ package list;
  * @Author: luweihong
  * @Date: 2021/4/6
  * 链表翻转
+ * 输入: 1->2->3->4->5->NULL
+ * 输出: 5->4->3->2->1->NULL
  */
 public class Reverse {
 
 
+    public static ListNode reverseList(ListNode head) {
+        ListNode reverse = null;
+        while (head != null){
+            ListNode newHead = new ListNode(head.val);
+            newHead.next = reverse;
+            reverse = newHead;
+            head = head.next;
+        }
+
+        return reverse;
+    }
+
+    public static void main(String[] args) {
+        reverseList(ListNode.simpleListNode());
+    }
+
 }
+
