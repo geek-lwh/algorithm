5c5f665afd1f79fb5198759647464dcc9598973b
luweihong <luweihong@ahaschool.com>
2021-04-08 15:36:05 +0800

    环形列表

--- src/main/java/list/ListNode.java
+++ src/main/java/list/ListNode.java
@@ -23,6 +23,24 @@ public class ListNode {
         return listNode1;
     }
 
+    public static ListNode cycleListNode(){
+        ListNode listNode1 = new ListNode(1);
+        ListNode listNode2 = new ListNode(2);
+        ListNode listNode3 = new ListNode(3);
+        ListNode listNode4 = new ListNode(4);
+        ListNode listNode5 = new ListNode(5);
+        ListNode listNode6 = new ListNode(6);
+        ListNode listNode7 = new ListNode(7);
+        listNode1.next = listNode2;
+        listNode2.next = listNode3;
+        listNode3.next = listNode4;
+        listNode4.next = listNode5;
+        listNode5.next = listNode6;
+        listNode6.next = listNode7;
+        listNode7.next = listNode1;
+
+        return listNode1;
+    }
 
     public static void print(ListNode listNode){
         while (listNode != null){
@@ -30,4 +48,6 @@ public class ListNode {
             listNode = listNode.next;
         }
     }
+
+
 }
