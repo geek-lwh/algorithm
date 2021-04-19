1d052e6256f7b29e63b9f8d025dd064ec2c380eb
luweihong <luweihong@ahaschool.com>
2021-04-19 15:48:56 +0800

    增加创建数组的方法

--- src/main/java/sort/ArrayFactory.java
+++ src/main/java/sort/ArrayFactory.java
@@ -9,10 +9,19 @@ import java.util.Arrays;
 public class ArrayFactory {
 
     public static int[] forSort(){
-        return new int[]{7,8,3,5,1,6,5,9,12,15,54,24,4};
+        return new int[]{7,8,3,5,1,6,5,9,12,15,54,24,4,77,33,11,23,45,66,1};
+    }
+
+    public static int[] newArray(int ... num){
+        int[] arr = new int[num.length];
+        for(int i = 0 ;i < num.length;i++){
+            arr[i] = num[i];
+        }
+
+        return arr;
     }
 
     public static void print(int[] arr){
-        Arrays.stream(arr).forEach(System.out::println);
+        System.out.println("排序后：" + Arrays.toString(arr));
     }
 }
